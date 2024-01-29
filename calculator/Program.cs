a66ee0c825dbc9510fbe2e1a31ade278496968a0
kon1ac <144265321+kon1ac@users.noreply.github.com>
2024-01-29 20:09:10 +0300

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,112 +1,47 @@
-#include <iostream>
-#include <cmath>
-#include <Windows.h>
-using namespace std;
+package org.example;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-int main() 
-{
-    SetConsoleCP(1251);
-    setlocale(0, "Russian");
-    int choice;
-    double a, b, result;
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Создаение объекта FileWriter для записи данных в файл
+            FileWriter writer = new FileWriter("C:\\Users\\kostu\\OneDrive\\Рабочий стол\\students.txt");
 
-    do 
-    {
-        cout << "1. Сложение" << endl;
-        cout << "2. Вычитание" << endl;
-        cout << "3. Умножение" << endl;
-        cout << "4. Деление" << endl;
-        cout << "5. Возведение в степень" << endl;
-        cout << "6. Нахождение квадратного корня" << endl;
-        cout << "7. Нахождение 1 процента от числа" << endl;
-        cout << "8. Найти факториал числа" << endl;
-        cout << "9. Выйти из программы" << endl;
-        cout << "Выберите операцию: ";
-        cin >> choice;
+            // Создание объекта Scanner для чтения данных с консоли
+            Scanner scanner = new Scanner(System.in);
 
-        switch (choice) 
-        {
-        case 1:
-            cout << "Введите два числа, которые хотите сложить: ";
-            cin >> a >> b;
-            result = a + b;
-            cout << "\nРезультат: " << result << endl;
-            break;
-        case 2:
-            cout << "Введите два числа, которые хотите вычесть: ";
-            cin >> a >> b;
-            result = a - b;
-            cout << "\nРезультат: " << result << endl;
-            break;
-        case 3:
-            cout << "Введите два числа, которые хотите умножить: ";
-            cin >> a >> b;
-            result = a * b;
-            cout << "\nРезультат: " << result << endl;
-            break;
-        case 4:
-            cout << "Введите два числа, которые хотите разделить: ";
-            cin >> a >> b;
-            if (b != 0) 
-            {
-                result = a / b;
-                cout << "\nРезультат: " << result << endl;
+            // Запрос  информации о студентах у пользователя
+            System.out.print("Введите количество студентов: ");
+            int count = scanner.nextInt();
+            scanner.nextLine(); // Считываем лишний символ переноса строки после ввода числа
+
+            for (int i = 1; i <= count; i++) {
+                System.out.println("Введите информацию о студенте " + i + ":");
+                System.out.print("Имя: ");
+                String name = scanner.nextLine();
+                System.out.print("Возраст: ");
+                int age = scanner.nextInt();
+                System.out.print("Средний балл: ");
+                double averageGrade = scanner.nextDouble();
+                scanner.nextLine(); // Считываем лишний символ переноса строки после считывания числа
+
+                // Записываем информацию о студенте в файл
+                writer.write("Студент " + i + ": " + name + ", Возраст: " + age +
+                        ", Средний балл: " + averageGrade + System.lineSeparator());
             }
-            else 
-            {
-                cout << "\nОшибка! Нельзя делить на ноль." << endl;
-            }
-            break;
-        case 5:
-            cout << "Введите число и степень, в которую хотите возвести: ";
-            cin >> a >> b;
-            result = pow(a, b);
-            cout << "\nРезультат: " << result << endl;
-            break;
-        case 6:
-            cout << "Введите число, для которого хотите найти квадратный корень: ";
-            cin >> a;
-            if (a >= 0) 
-            {
-                result = sqrt(a);
-                cout << "\nРезультат: " << result << endl;
-            }
-            else {
-                cout << "\nОшибка! Нельзя извлечь корень из отрицательного числа." << endl;
-            }
-            break;
-        case 7:
-            cout << "Введите число, для которого хотите найти 1 процент: ";
-            cin >> a;
-            result = a * 0.01;
-            cout << "\nРезультат: " << result << endl;
-            break;
-        case 8:
-            cout << "Введите число, для которого хотите найти факториал: ";
-            cin >> a;
-            if (a >= 0) 
-            {
-                result = 1;
-                for (int i = 1; i <= a; i++) {
-                    result *= i;
-                }
-                cout << "\nРезультат: " << result << endl;
-            }
-            else 
-            {
-                cout << "Ошибка. Факториал определен только для неотрицательных чисел." << endl;
-            }
-            break;
-        case 9:
-            cout << "\nПрограмма завершена." << endl;
-            break;
-        default:
-            cout << "\nОшибка! Неверный выбор операции." << endl;
-            break;
+
+            // Закрываем файл после записи данных
+            writer.close();
+
+            // Вывод сообщение об успешной записи данных
+            System.out.println("Данные успешно записаны в файл students.txt");
+
+            // Открытие файла для проверки записанных данных
+
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи данных в файл: " + e.getMessage());
         }
-        cout << endl;
-
-    } while (choice != 9);
-
-    return 0;
+    }
 }
